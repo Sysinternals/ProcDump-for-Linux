bd3ce9f40b267561625a314f73566d80e3109322
Javid Habibi <javid.habibi@microsoft.com>
2020-04-08 11:55:37 -0700

    updated copyright and minimum OS

--- src/ProcDumpConfiguration.c
+++ src/ProcDumpConfiguration.c
@@ -834,7 +834,7 @@ bool CheckKernelVersion()
 void PrintBanner()
 {
     printf("\nProcDump v1.1.1 - Sysinternals process dump utility\n");
-    printf("Copyright (C) 2019 Microsoft Corporation. All rights reserved. Licensed under the MIT license.\n");
+    printf("Copyright (C) 2020 Microsoft Corporation. All rights reserved. Licensed under the MIT license.\n");
     printf("Mark Russinovich, Mario Hewardt, John Salem, Javid Habibi\n");
 
     printf("Monitors a process and writes a dump file when the process exceeds the\n");
