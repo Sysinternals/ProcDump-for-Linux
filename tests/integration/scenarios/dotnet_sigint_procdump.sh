6858711babfdf4fa2b7da4138d4d361846940e79
Mario Hewardt <marioh@microsoft.com>
2022-11-18 18:14:11 -0800

    added new integration tests

---
+++ tests/integration/scenarios/dotnet_sigint_procdump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+PROCDUMPPATH=$(readlink -m "$DIR/../../../bin/procdump");
+
+pushd .
+cd TestWebApi
+dotnet run --urls=http://localhost:5032 > /dev/null 2>&1&
+TESTPID=$!
+sleep 5s
+sudo $PROCDUMPPATH -e TestWebApi testdump&
+PDPID=$!
+sudo kill -2 ${PDPID}
+popd
+sleep 5s
+
+#check to make sure profiler so is unloaded
+PROF="$(cat /proc/${TESTPID}/maps | awk '{print $6}' | grep '\procdumpprofiler.so' | uniq)"
+pkill -9 TestWebApi
+if [[ "$PROF" == "procdumpprofiler.so" ]]; then
+    exit 1
+else
+    exit 0
+fi
