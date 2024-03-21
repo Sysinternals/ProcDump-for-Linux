1641a1f9264191416f019414ac2968c04f31fae6
Mario Hewardt <marioh@microsoft.com>
2024-03-21 12:15:28 -0700

    Update build dependencies

--- .devcontainer/install-ubuntu-dependencies.sh
+++ .devcontainer/install-ubuntu-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
-DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt -y install software-properties-common
-add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
-apt-get update
-apt upgrade -y \
+echo "APT::Get::Assume-Yes \"true\";" > sudo /etc/apt/apt.conf.d/90assumeyes
+sudo DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt -y install software-properties-common
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt-get update
+sudo apt upgrade -y \
 && apt-get install -y --no-install-recommends \
     ca-certificates \
     curl \
@@ -35,22 +35,22 @@ apt upgrade -y \
     libbpf-dev
 
 # Set preference to clang-12
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-12 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-12 100
 
 # Build and install bpftool
-rm -rf /usr/sbin/bpftool
-git clone --recurse-submodules https://github.com/libbpf/bpftool.git
-cd bpftool/src
-make install
-ln -s /usr/local/sbin/bpftool /usr/sbin/bpftool
+sudo rm -rf /usr/sbin/bpftool
+sudo git clone --recurse-submodules https://github.com/libbpf/bpftool.git
+sudo cd bpftool/src
+sudo make install
+sudo ln -s /usr/local/sbin/bpftool /usr/sbin/bpftool
 
 # install debbuild
-wget https://github.com/debbuild/debbuild/releases/download/22.02.1/debbuild_22.02.1-0ubuntu20.04_all.deb \
+sudo wget https://github.com/debbuild/debbuild/releases/download/22.02.1/debbuild_22.02.1-0ubuntu20.04_all.deb \
     && dpkg -i debbuild_22.02.1-0ubuntu20.04_all.deb
 
 # install .NET 6 for signing process and integration tests
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-apt -y update && apt-get install -y dotnet-runtime-6.0
-apt-get install -y dotnet-sdk-6.0
+sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo rm packages-microsoft-prod.deb
+sudo apt -y update && apt-get install -y dotnet-runtime-6.0
+sudo apt-get install -y dotnet-sdk-6.0
